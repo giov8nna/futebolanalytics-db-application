94e462c1e3faeacfd2259aba53150378185b39b9
ReiVedd <130795713+ReiVedd@users.noreply.github.com>
2026-05-27 23:28:39 -0300

    Insert position data into posicoes table

---
+++ posicoes.sql
@@ -0,0 +1,12 @@
+USE futebol_analytics;
+
+insert into posicoes (nome_posicao) values
+
+('Goleiro'), 
+('Zagueiro'),
+('Lateral Direito'),
+('Lateral Esquerdo'),
+('Volante'),
+('Meia'),
+('Atacante'),
+('Centroavante');
