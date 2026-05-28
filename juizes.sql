fa9f281e79ca1f28ef1405dcdf86752f21b34f80
ReiVedd <130795713+ReiVedd@users.noreply.github.com>
2026-05-27 23:29:24 -0300

    Colocando os juizes no sql

---
+++ juizes.sql
@@ -0,0 +1,37 @@
+INSERT IGNORE INTO juizes (nome, nacionalidade) VALUES 
+
+
+('Renan Novaes Insabralde', 'Brasileira'),
+('Felipe da Silva Gonçalves Paludo', 'Brasileira'),
+('Rodrigo Batista da Silva', 'Brasileira'),
+('Júlio César de Oliveira', 'Brasileira'),
+('João Vitor Gobi', 'Brasileira'),
+('Rafael Martins de Oliveira', 'Brasileira'),
+('Vinícius Gomes do Amaral', 'Brasileira'),
+('Gustavo Holanda Souza', 'Brasileira'),
+('Murilo Ugolini Pelegrini', 'Brasileira'),
+('Leonardo Sígari Zanon', 'Brasileira'),
+('Gabriel Henrique Meira Bispo', 'Brasileira'),
+('Augusto Domingos Borges Ortega', 'Brasileira'),
+('Halbert Luis Moraes Baia', 'Brasileira'),
+('William Machado Steffen', 'Brasileira'),
+('Jonathan Benkenstein Pinheiro', 'Brasileira'),
+('Tiago Augusto Kappes Diel', 'Brasileira');
+
+INSERT INTO juizes (nome, nacionalidade) VALUES
+('Everton Moreira Prates', 'Brasileira'),-- Apitou Manauara x Rio Branco-AC
+('Moisés Estevão de Moura', 'Brasileira'),-- Apitou Humaitá x Porto Velho
+('Dagoberto Silva Modesto', 'Brasileira'),-- Apitou Trem x Manaus
+('Rafael Santos de Andrade', 'Brasileira'),-- Apitou Princesa x São Raimundo-RR
+('Antonio Carlos Pequeno Frutuoso', 'Brasileira'),-- Apitou River x Águia de Marabá
+('Julian Negreiros de Castro', 'Brasileira'),-- Apitou Fluminense-PI x Maranhão
+('Wallas Martins Lopes', 'Brasileira'),-- Apitou Moto Club x Tocantinópolis
+('Cianã da Silva Sousa', 'Brasileira'),-- Apitou Cametá x Altos
+('Marianna Nanni Batalha', 'Brasileira'),-- Apitou Santa Cruz-RN x Treze
+('José Jaini Oliveira Bispo', 'Brasileira'),-- Apitou Atlético-CE x Potiguar
+('Thayslane de Melo Costa', 'Brasileira'),-- Apitou Maracanã x América-RN
+('Cesar Pereira Leite', 'Brasileira'),-- Apitou Sousa x Iguatu
+('Paulo Belence Alves dos Prazeres Filho', 'Brasileira'), -- Apitou Sergipe x ASA
+('Jonata de Souza Gouveia', 'Brasileira'),-- Apitou Juazeirense x Itabaiana
+('José Ricardo Vasconcellos Laranjeira', 'Brasileira'), -- Apitou Petrolina x Jacuipense
+('Michelangelo Martins de Almeida Junior', 'Brasileira'); -- Apitou CSE x Retrô
